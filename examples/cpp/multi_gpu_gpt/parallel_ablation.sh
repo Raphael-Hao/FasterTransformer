0ffec34abc50708d76a9eddf6fd716d60dfd102f
Raphael-Hao <raphaelhao@outlook.com>
2023-08-19 18:29:18 +0800

    update

--- examples/cpp/multi_gpu_gpt/parallel_ablation.sh
+++ examples/cpp/multi_gpu_gpt/parallel_ablation.sh
@@ -91,9 +91,10 @@ sed -i "s/use_ffn=.*/use_ffn=$use_ffn/g" parallel_ablation_"$used_gpus".ini
 sed -i "s/tensor_para_size=.*/tensor_para_size=$used_gpus/g" parallel_ablation_"$used_gpus".ini
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-echo "$SCRIPT_DIR" >dir.txt
-echo "$PATH" >>dir.txt
-printenv >env.txt
+
 # cd to build directory run parallel_ablation and cd back to examples/cpp/multi_gpu_gpt
-cd ../../../build &&
-    mpirun -n "$used_gpus" ./bin/parallel_ablation ../examples/cpp/multi_gpu_gpt/parallel_ablation_"$used_gpus".ini ../examples/cpp/multi_gpu_gpt/start_ids_"$used_gpus".csv
+cd "$SCRIPT_DIR"/../../../build || exit
+# list all files in bin and echo them to a file
+ls ./bin >../examples/cpp/multi_gpu_gpt/bin_list.txt
+
+mpirun -n "$used_gpus" ./bin/parallel_ablation ../examples/cpp/multi_gpu_gpt/parallel_ablation_"$used_gpus".ini ../examples/cpp/multi_gpu_gpt/start_ids_"$used_gpus".csv
